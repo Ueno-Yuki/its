1828331e12830f2bd40f8cf86833126c8afe2fcb
yuki_ueno <jam14_01@icloud.com>
2023-03-05 13:59:07 +0900

    validation

--- src/main/resources/templates/issues/IssueForm.java
+++ src/main/resources/templates/issues/IssueForm.java
@@ -1,9 +1,16 @@
 package templates.issues;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class IssueForm {
+	@NotBlank
+	@Size(max=256)
 	private String summary;
+
+	@NotBlank
+	@Size(max=256)
 	private String description;
 }
